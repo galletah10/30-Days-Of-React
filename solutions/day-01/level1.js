852a9ce44e3207522546a906e053a16ddeb8caa9
Enrique Orea <orea_96@hotmail.com>
2022-01-31 11:45:13 -0600

    primer ejercicio

---
+++ solutions/day-01/level1.js
@@ -0,0 +1,18 @@
+const emptyArray = [];
+
+const fiveElementArray = [1, 2, 4, 5, 6, 7, 8];
+
+fiveElementArray.length();
+
+console.log(fiveElementArray[0]);
+console.log(fiveElementArray[0]);
+
+
+function fullName(firstName, lastName){
+    return `Your full name is ${firstName}, ${lastName}`;
+}
+
+function addNumber(num1, num2){
+    return num1 + num2;
+}
+
